8e511046b9b4cccc4a5986ffd84fe68ed1dfb0fd
Les Matheson <lmatheson4@bloomberg.net>
2020-12-02 05:53:32 -0500

    Add hello-basic-logging.py

---
+++ basic/hello-basic-logging.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3.8
+
+import os
+import logging
+
+
+app_name = __name__
+logfile=f"{os.environ.get('TMPDIR','/tmp')}/{app_name}.log"
+logging.basicConfig(filename=logfile,level=logging.DEBUG)
+
+
+logging.debug("Hello world")
+
+# Don't forget to shutdown to flush the events out
+logging.shutdown()
+
+print(f"Log written to {logfile}, contents:")
+with open(logfile,'r') as lf:
+    print(lf.read())
+
